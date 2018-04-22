79a02deb658f788ed2f58df84a5f0e48761af240
Franchize <tracyhfrancis@hawk.iit.edu>
2018-04-21 19:04:32 -0500

    working code

--- server/Yargs/yargs.js
+++ server/Yargs/yargs.js
@@ -1,44 +1,40 @@
-const argv = require('yargs').argv; //helps you build interactive command line tools by parsing arguments
+var argv = require('yargs').argv;
 const fs = require('fs');
-const readline = require('readline'); //asks questions via cmd and receives a callback which it processes
+const readline = require('readline');
 
-//two flags, one to read the file and the file name
-if(argv._[0] == 'read' && argv.fileName !=undefined){
-   fs.readFile(argv.fileName,(err, data)=>{
-      if(err){
-         return console.log('Error reading file: ', argv.fileName);
-      }
-      console.log(data.toString());
-   })
-}else if (argv._[0] == 'delete' && argv.filename != undefined) {
-   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-   });
-   rl.question('Are you sure you want to delete file ' + argv.fileName + '?', (answer)=>{
-      var deleted = true;
-      if (answer == 'Y' || answer == 'y'){
-         fs.unlink(argv.fileName, (err)=>{
-            if(err){
-               console.log('Error deleting file: ', argv.fileName);
-               rl.close();
-               deleted = false;
-            }
-            if (deleted == true){
-               console.log('File ' + argv.fileName + ' has been successfully deleted.');
-            }
-               rl.close();
-         })
-      }
-      else{
-         rl.close();
-      }
-   })
+if(argv._[0] == 'read' && argv.fileName!=undefined){
+    fs.readFile(argv.fileName,(err, data)=>{
+        if(err){
+            return console.log('error reading file', argv.fileName);
+        }
+        console.log(data.toString());
+    })
 
+}else if(argv._[0]=='delete' && argv.fileName != undefined){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Are you sure you wanna delete:'+argv.fileName+'?',(answer)=>{
+        var deleted = true;
+        if(answer == 'Y'|| answer == 'y'){
+            fs.unlink(argv.fileName,(err)=>{
+                if(err){
+                    console.log('Error deleting file', argv.fileName);
+                    rl.close();
+                    deleted = false;
+                }
+                if(deleted == true){
+                    console.log('Completed deleting file:', argv.fileName);
+                }
+                rl.close();
+            })
+        }else{
+            rl.close();
+        }
+    })
 }else{
-   console.log('Incorrect command');
+    console.log('Incorrect command');
 }
 
 //console.log(argv);
-
-//in the command line, run node yargs.js read <or try delete> --fileName=read.txt
